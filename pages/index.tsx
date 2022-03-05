675672f93abe6ece526e349ea54638ff92610d68
kstoqnov <kirilstoqnov@gmail.com>
2022-03-05 17:48:58 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <h1>This is example how work react-query nothing more .. :) </h1>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          listStyle: "none",
+          border: "1px solid black",
+          borderRadius: "5px",
+          padding: "10px",
+          width: "500px",
+          fontSize: "1.8rem",
+        }}
+      >
+        <li style={{ marginBottom: "10px" }}>
+          <Link href="/react-query">
+            <a>React Query</a>
+          </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <Link href="/fetch">
+            <a>Fetch</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
